15dbc6eaf363e156d80c4a4e126570e667258683
Mateusz Wasil <wasil.mateusz@gmail.com>
2020-05-19 12:22:49 +0200

    move styles from public to src folder - fixed problem with no style

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Web from './components/Web';
 import * as serviceWorker from './serviceWorker';
+import './styles.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
